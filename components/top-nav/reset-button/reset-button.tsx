7d1f76112f0f71522f89b92a71de1b674f57c0dd
mattRightCT <matt@rightct.co>
2021-03-03 10:24:28 -0500

    refactor: making our top nav buttons way more flexible -- should probably rename stuff too at some point

--- components/top-nav/reset-button/reset-button.tsx
+++ components/top-nav/reset-button/reset-button.tsx
@@ -1,20 +1,44 @@
-import React from 'react'
-import GameStateService from '../../game/services/game-state.service';
+import React, { useEffect, useState } from 'react';
+import ButtonService from '../../services/button.service';
+import { Subscription } from 'rxjs';
 
 /**
  * @method
  * @description
- * Wrapper function to undo the latest move
+ * Wrapper function to click this button
  **/
-function reset(): void {
-  GameStateService.resetGameState();
+function button1Click(): void {
+  ButtonService.clickButton1();
 }
 
 const ResetButtonComponent = () => {
+  // Keeping an eye on getting our text
+  const [text, setText] = useState<string>('');
+  /**
+   * @method
+   * @description
+   * Keeping an eye on whether the menu is open or not
+   **/
+  useEffect(() => {
+    const allSubs: Subscription = new Subscription();
+    // Listening for changes to the current score
+    allSubs.add(ButtonService.navButton1Text.subscribe(
+      (newText: string) => {
+        setText(newText);
+      }
+    ));
+    // Unsubscribe when we are finished
+    return function cleanup() {
+      allSubs.unsubscribe();
+    };
+  });
+  
   return (
-    <button className='nav' type="button" onClick={reset}>
-      Reset
-    </button>
+    text.length > 0?
+      <button className='nav' type="button" onClick={button1Click}>
+        {text}
+      </button>
+      : <div></div>
   );
 }
 
