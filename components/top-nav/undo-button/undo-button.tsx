334c73c48338d932b8125f0df711b4f17ff5b86a
mattRightCT <matt@rightct.co>
2021-03-03 10:25:00 -0500

    refactor: making our top nav buttons way more flexible -- should probably rename stuff too at some point

--- components/top-nav/undo-button/undo-button.tsx
+++ components/top-nav/undo-button/undo-button.tsx
@@ -1,20 +1,44 @@
-import React from 'react'
-import GameStateService from '../../game/services/game-state.service';
+import React, { useEffect, useState } from 'react';
+import { Subscription } from 'rxjs';
+import ButtonService from '../../services/button.service';
 
 /**
  * @method
  * @description
- * Wrapper function to undo the latest move
-**/
-function undo(): void {
-  GameStateService.undoLatestMove();
+ * Wrapper function to click this button
+ **/
+function button2Click(): void {
+  ButtonService.clickButton2();
 }
 
 const UndoButtonComponent = () => {
+  // Keeping an eye on getting our text
+  const [text, setText] = useState<string>('');
+  /**
+   * @method
+   * @description
+   * Keeping an eye on whether the menu is open or not
+   **/
+  useEffect(() => {
+    const allSubs: Subscription = new Subscription();
+    // Listening for changes to the current score
+    allSubs.add(ButtonService.navButton2Text.subscribe(
+      (newText: string) => {
+        setText(newText);
+      }
+    ));
+    // Unsubscribe when we are finished
+    return function cleanup() {
+      allSubs.unsubscribe();
+    };
+  });
+  
   return (
-    <button className='nav' type="button" onClick={undo}>
-      Undo
-    </button>
+    text.length > 0?
+      <button className='nav' type="button" onClick={button2Click}>
+        {text}
+      </button>
+      : <div></div>
   );
 }
 
